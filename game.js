b05d4b3083acfb4359e71cd0ee3fe84456e74ec5
renatasellanes <renatasellanes@hotmail.com>
2022-11-03 22:13:35 -0300

    version 2

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 // Selecionando os componentes:
 
 let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
 let cactosBig = document.querySelector("#cactosBig");
-let passaro = document.querySelector("#passaro");
 let ceu = document.querySelector("#ceu");
 let terra = document.querySelector("#terra");
 let score = document.querySelector("#score");
@@ -10,45 +10,63 @@ let gameOver = document.querySelector("#gameOver");
 
 
 // Declarando as variaveis do score:
-let interval = null;
+let interval = 0;
 let playerScore = 0;
 
 // função Score:
-let scoreCounter = () => {
+const scoreCounter = () => {
     playerScore++;
     score.innerHTML = `Score <b>${playerScore}</b>`;
 }
 
-interval = setInterval(scoreCounter,200);
 
 //StartGame
 
 window.addEventListener("keydown", (start)=> {
-    console.log(start.code)
 
-    if(start.code == "AltLeft")
+
+    if(start.code == "Enter")
     {
         gameOver.style.display = "none";
         cactosBig.classList.add("movimentoCactusBig");
-        passaro.classList.add("movimentoPassaro");
-        terra.firstElementChild.style.animation == "terraAnimate 1.5s linear infinite";
-        ceu.firstElementChild.style.animation == "ceuAnimate 50s linear infinite";
+        terra.classList.add("moviemntaTerra");
+        ceu.classList.add("movimentaCeu");
 
         //score
         let playerScore = 0;
         interval = setInterval(scoreCounter, 200);
 
     }
+
 });
 // Jump Dino
-const dino = document.querySelector("#dino");
+window.addEventListener("keydown", (e)=>{
+    if(e.key == "Enter")
+    if(dino.classList != "jumpDino") {
+        dino.classList.add("jumpDino");
+        setTimeout(()=> {
+            dino.classList.remove("jumpDino")
 
-const jump = () => {
-    dino.querySelector.add(".jumpDino")
+        },500)
+    }
+});
+//Game Over
+let result = setInterval(()=> {
 
-    setTimeout (() => {
-        dino.querySelector.remove(".jumpDino"), 500
-    })
-}
+    const cactosPosition =+window.getComputedStyle(cactosBig).left.replace('px','')
+    const dinoPosition = +window.getComputedStyle(dino).bottom.replace('px', '')
 
-document.addEventListener("onclick", jump);
+    if ( cactosPosition <=50 && cactosPosition > 0 && dinoPosition <=80) {
+        //console.log("Game Over")
+
+        
+        gameOver.style.display="block";
+        cactosBig.classList.remove("movimentoCactusBig");
+        terra.classList.remove("moviemntaTerra");
+        ceu.classList.remove("movimentaCeu");
+        clearInterval(interval);
+        playerScore = 0;
+
+    }
+
+},10);
